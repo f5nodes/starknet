50a9a2a666eaaf5e9d0cf55d7dec68513b7363c6
vasylcode <vasylcode@gmail.com>
2022-06-29 15:30:25 +0200

    update & install

--- setup.sh
+++ setup.sh
@@ -1,1 +1,31 @@
 #!/bin/bash
+
+node_install=""
+node_update=""
+
+if [ "$language" = "uk" ]; then
+	PS3='Виберіть опцію: '
+	options=("Встановити ноду" "Оновити ноду" "Вийти з меню")
+	selected="Ви вибрали опцію"
+else
+	PS3='Enter your option: '
+	options=("Install the node" "Update the node" "Quit")
+	selected="You choose the option"
+fi
+
+select opt in "${options[@]}"
+do
+    case $opt in
+        "${options[0]}")
+            echo "$selected $opt"
+            ;;
+        "${options[1]}")
+            echo "$selected $opt"
+            ;;
+        "${options[2]}")
+			echo "$selected $opt"
+            break
+            ;;
+        *) echo "unknown option $REPLY";;
+    esac
+done
---
+++ update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/env
+rustup update stable
+rustc -V
+cd ~/pathfinder
+git fetch
+git checkout v0.2.1-alpha
+cargo build --release --bin pathfinder
+mv ~/pathfinder/target/release/pathfinder /usr/local/bin/
+cd py
+source .venv/bin/activate
+PIP_REQUIRE_VIRTUALENV=true pip install -r requirements-dev.txt
+systemctl restart starknetd
+
+if [ "$language" = "uk" ]; then
+	echo -e "\n\033[0;94mНода успішно оновлена до актуальної версії!"
+	echo -e "\033[0;93mПоточна версія ноди:"
+else
+	echo -e "\n\033[0;94mNode was successfully updated!"
+	echo -e "\033[0;93mCurrent node version:"
+fi
+pathfinder -V
