4231574ed08bb3c1ae24b745d2fbfa15d99582dc
vasylcode <vasylcode@gmail.com>
2022-07-05 12:42:09 +0200

    v0.2.4-alpha

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ rustup update stable
 rustc -V
 cd ~/pathfinder
 git fetch
-git checkout v0.2.3-alpha
+git checkout v0.2.4-alpha
 cargo build --release --bin pathfinder
 mv ~/pathfinder/target/release/pathfinder /usr/local/bin/
 cd py
