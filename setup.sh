e769a0270e4c3a7783e80dcafe9eaf35b3ebd5d2
vasylcode <vasylcode@gmail.com>
2023-01-07 00:01:40 +0100

    new option added

--- setup.sh
+++ setup.sh
@@ -2,14 +2,15 @@
 
 node_install="https://raw.githubusercontent.com/f5nodes/starknet/main/install.sh"
 node_update="https://raw.githubusercontent.com/f5nodes/starknet/main/update.sh"
+clearDB_install="https://raw.githubusercontent.com/f5nodes/starknet/main/starknet_clear_install.sh"
 
 if [ "$language" = "uk" ]; then
 	PS3='Виберіть опцію: '
-	options=("Встановити ноду" "Оновити ноду" "Вийти з меню")
+	options=("Встановити ноду" "Оновити ноду" "Встановити скрипт авто очищення бази даних " "Вийти з меню")
 	selected="Ви вибрали опцію"
 else
     PS3='Enter your option: '
-    options=("Install the node" "Update the node" "Quit")
+    options=("Install the node" "Update the node" "Install & run auto clear DB script" "Quit")
     selected="You choose the option"
 fi
 
@@ -29,6 +30,12 @@ do
             break
             ;;
         "${options[2]}")
+            echo "$selected $opt"
+            sleep 1
+            . <(wget -qO- $clearDB_install)
+            break
+            ;;    
+        "${options[3]}")
 			echo "$selected $opt"
             break
             ;;
