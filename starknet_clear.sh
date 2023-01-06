175a00d4657c327fab55a64aee2fac5be101184b
vasylcode <vasylcode@gmail.com>
2023-01-06 22:01:55 +0100

    starknet clear DB script

---
+++ starknet_clear.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while true; do
+    # Check if the size of the DB is over 100 GB
+    if [ $(du -h pathfinder/pathfinder/goerli.sqlite | cut -f1 | sed 's/G//') -gt 100 ]; then
+        # Clear database and restart the node
+        cd $HOME/pathfinder/
+        docker-compose down -v
+        rm -rf $HOME/pathfinder/pathfinder
+        mkdir -p $HOME/pathfinder/pathfinder
+        chown -R 1000.1000 .
+        docker-compose up -d
+    fi
+    # Wait 1 hour
+    sleep 3600
+done
+
