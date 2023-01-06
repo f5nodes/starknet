27750c16643b8b2cea3dae6d590601ea06fb60ee
vasylcode <vasylcode@gmail.com>
2023-01-07 00:15:23 +0100

    starknet clear script upd

--- starknet_clear_install.sh
+++ starknet_clear_install.sh
@@ -2,10 +2,9 @@
 
 starknet_clear="https://raw.githubusercontent.com/f5nodes/starknet/main/starknet_clear.sh"
 
-cd $HOME
-wget -q starknet_clear
+wget -q $starknet_clear -O $HOME/starknet_clear.sh
 sudo chmod +x $HOME/starknet_clear.sh
-nohup $HOME/starknet_clear.sh > starknet_clear.out &
+nohup $HOME/starknet_clear.sh > starknet_clear.out 2> /dev/null &
 
-echo -e "\nStarknet clear DB script was installed & running!\e[0m!"
+echo -e "\nStarknet clear DB script was installed & running\e[0m!"
 echo -e "\nCheck the output of running script (once a hour) \e[92mcat starknet_clear.out\e[0m"
